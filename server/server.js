c07a1fada11d1efd28b4ec205b8ac24623c067c8
Shane Ruggirello <darkgasher2794@gmail.com>
2026-03-03 22:42:35 -0500

    Added a very basic frontend and adjusted the server.js script slightly.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
-import constructorMethod from './routes/index.js';
 
-app.use(express.json());
-constructorMethod(app);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+//import constructorMethod from './routes/index.js';
+
+//app.use(express.json());
+//constructorMethod(app);
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
